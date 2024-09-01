a5f92d92b59cdfcfb598af3d9e9c4e8bda44c880
zainaly0 <zaidk4076@gmail.com>
2024-09-01 21:24:22 +0530

    debugging

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,21 +2,32 @@ package Sorting;
 
 class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {7, 8, 1, 3, 2};
+        int[] arr = { 7, 8, 1, 3, 2 };
 
-        for(int a = 0; a<arr.length; a++){
-            for(int b = 0; b<arr.length-1-a; b++){
-                if(arr[b] >arr[b+1]){
+        for (int a = 0; a < arr.length - 1; a++) {
+            int flag = 0;
+            // System.out.println("top outer flag " + flag+ " a: " + a);
+            for (int b = 0; b < arr.length - 1 - a; b++) {
+                if (arr[b] > arr[b + 1]) {
+                    // Swap arr[b] and arr[b + 1]
                     int temp = arr[b];
-                    arr[b] = arr[b+1];
-                    arr[b+1] = temp;
+                    arr[b] = arr[b + 1];
+                    arr[b + 1] = temp;
+                    flag = 1;
+                    // System.out.println(" inner flag " + flag+ " b: " + b + " a: " + a);
                 }
+            }
+            // System.out.println("bottom outer flag " + flag+ " a: " + a);
+
+            // If no two elements were swapped in the inner loop, the array is sorted
+            if (flag == 0) {
+                break;
             }
         }
 
-        for(int a=0; a<arr.length; a++){
+        // Print sorted array
+        for (int a = 0; a < arr.length; a++) {
             System.out.println(arr[a]);
         }
     }
-
 }
