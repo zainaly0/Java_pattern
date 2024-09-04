e81ae29ba3c7d4a5011eeb4a0bf9cd8ebfad265b
zainaly0 <zaidk4076@gmail.com>
2024-09-04 23:35:59 +0530

    update java

---
+++ Recursion/NumberPrint.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class NumberPrint{
+
+    public static void numPrint(int num){
+        if()
+    }
+
+    public static void main(String[] args){
+        numPrint(5)
+    }
+}
