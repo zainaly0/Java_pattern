c8e9b07e8d85aa93d422089413b43a778a54b056
zainaly0 <zaidk4076@gmail.com>
2024-09-14 00:04:23 +0530

    hashmap and OOPS in java

---
+++ OOPS/OOPS.java
@@ -0,0 +1,41 @@
+// package OOPS;
+
+class Pen{
+    String color;
+    String type;    //ballpoint gel
+
+    public void write(){
+        System.out.println("writting something");
+    }
+
+    public void printCOlor(){
+        System.out.println(this.color);
+    }
+}
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+
+public class OOPS {
+    public static void main(String[] args){
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+
+        pen1.printCOlor();
+
+        Pen pen2 = new Pen();
+        pen2.color = "red";
+        pen2.type = "well";
+        pen2.printCOlor();
+
+    }
+}
