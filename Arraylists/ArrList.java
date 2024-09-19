5f28a73828462420d733423a7ed0aae5b2f967be
zainaly0 <zaidk4076@gmail.com>
2024-09-19 17:19:50 +0530

    array list in java

--- Arraylists/ArrList.java
+++ Arraylists/ArrList.java
@@ -1,5 +1,6 @@
 // package Arraylists;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class ArrList {
     public static void main(String[] args) {
@@ -11,9 +12,21 @@ public class ArrList {
         // System.out.println(list);
         // System.out.println(list.get(0));
         // System.out.println(list.set(0, 23));
-        System.out.println(list);
+        // System.out.println(list);
         list.add(0, 1); //for adding by index 
         list.remove(2);
+
+        // arraylist size
+        list.size();
+
+
+        //loop in arrayList
+        for(int a = 0; a<list.size();a++){
+            System.out.println(list.get(a));
+        }
+
+        // sorting 
+        Collection.sort(list);
 
         
     }
