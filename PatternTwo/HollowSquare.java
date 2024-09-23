16eb640a633201d6248d2001e871c4ecd8dd05fa
zainaly0 <zaidk4076@gmail.com>
2024-09-23 23:51:32 +0530

    hollow square in java pattern

---
+++ PatternTwo/HollowSquare.java
@@ -0,0 +1,16 @@
+public class HollowSquare {
+    public static void main(String[] args){
+        int n = 5;
+        int m = 5;
+        for(int a = 1; a<=n; a++){
+            for(int b = 1; b<=m; b++){
+                if(a == 1 || a==n || b== 1 || b==m){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
