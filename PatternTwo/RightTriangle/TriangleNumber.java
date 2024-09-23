f5958c6181bbba0c95e2cad1b1538df1612cdf65
zainaly0 <zaidk4076@gmail.com>
2024-09-24 00:21:31 +0530

    printed number 1 to 5 right triandgle

---
+++ PatternTwo/RightTriangle/TriangleNumber.java
@@ -0,0 +1,14 @@
+package RightTriangle;
+
+public class TriangleNumber {
+    public static void main(String[] args){
+        int n= 5;
+        int m = 5;
+        for(int a = 1; a<=n; a++){
+            for(int b = 1; b<=a; b++){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+}
