64146aa111bc30a7fbf1c7ce94a5c7759a5c5a2f
zainaly0 <zaidk4076@gmail.com>
2024-09-24 00:10:48 +0530

    diagonal line from left to right bottom and right to left bottom ABCDE print

---
+++ PatternTwo/X_DaigonalALPHA.java
@@ -0,0 +1,18 @@
+public class X_DaigonalALPHA {
+    public static void main(String[] args){
+        int n = 5;
+        int m = 5;
+        String str = "ABCDE";
+        for(int a = 1; a<=n; a++){
+            for(int b = 1; b<=m; b++){
+                if(a == b || a+b == 6){
+                    char ch= str.charAt(b-1);
+                    System.out.print(ch);
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
