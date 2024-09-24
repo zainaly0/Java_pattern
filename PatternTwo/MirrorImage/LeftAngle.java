00127917b5d27e91955855af15b6b22ee212f1af
zainaly0 <zaidk4076@gmail.com>
2024-09-24 14:25:20 +0530

    left angle triangle

--- PatternTwo/MirrorImage/LeftAngle.java
+++ PatternTwo/MirrorImage/LeftAngle.java
@@ -1,9 +1,20 @@
 package PatternTwo.MirrorImage;
 
 public class LeftAngle {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // this code is used for triangle and left and right
+
         int n = 5;
         int m = 5;
-        for(int a)
+        for (int a = 0; a < n; a++) {
+            for (int b = n - a; b > 0; b--) {
+                System.out.print("  ");
+            }
+
+            for (int b = 0; b <= a; b++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
